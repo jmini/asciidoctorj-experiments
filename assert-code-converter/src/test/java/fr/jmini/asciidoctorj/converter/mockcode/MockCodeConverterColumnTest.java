8461ee1e15bd57ae77464f145a15e2c460f0c470
Jeremie Bresson <dev@jmini.fr>
2017-11-26 21:30:09 +0100

    format code

--- assert-code-converter/src/test/java/fr/jmini/asciidoctorj/converter/mockcode/MockCodeConverterColumnTest.java
+++ assert-code-converter/src/test/java/fr/jmini/asciidoctorj/converter/mockcode/MockCodeConverterColumnTest.java
@@ -32,25 +32,25 @@ public class MockCodeConverterColumnTest {
     }
 
     // tag::generated-code[]
-public Column createMock() {
-Column mockColumn1 = mock(Column.class);
-when(mockColumn1.getId()).thenReturn(null);
-when(mockColumn1.getNodeName()).thenReturn(null);
-when(mockColumn1.getParent()).thenReturn(null);
-when(mockColumn1.getContext()).thenReturn(null);
-when(mockColumn1.getDocument()).thenReturn(null);
-when(mockColumn1.isInline()).thenReturn(false);
-when(mockColumn1.isBlock()).thenReturn(false);
-when(mockColumn1.getAttributes()).thenReturn(Collections.emptyMap());
-when(mockColumn1.getRoles()).thenReturn(Arrays.asList("r1", "r2", "r3"));
-when(mockColumn1.isReftext()).thenReturn(false);
-when(mockColumn1.getStyle()).thenReturn("STYLE");
-when(mockColumn1.getTable()).thenReturn(null);
-when(mockColumn1.getColumnNumber()).thenReturn(2);
-when(mockColumn1.getWidth()).thenReturn(550);
-when(mockColumn1.getHorizontalAlignment()).thenReturn(Table.HorizontalAlignment.CENTER);
-when(mockColumn1.getVerticalAlignment()).thenReturn(Table.VerticalAlignment.MIDDLE);
-return mockColumn1;
-}
- // end::generated-code[]
+    public Column createMock() {
+        Column mockColumn1 = mock(Column.class);
+        when(mockColumn1.getId()).thenReturn(null);
+        when(mockColumn1.getNodeName()).thenReturn(null);
+        when(mockColumn1.getParent()).thenReturn(null);
+        when(mockColumn1.getContext()).thenReturn(null);
+        when(mockColumn1.getDocument()).thenReturn(null);
+        when(mockColumn1.isInline()).thenReturn(false);
+        when(mockColumn1.isBlock()).thenReturn(false);
+        when(mockColumn1.getAttributes()).thenReturn(Collections.emptyMap());
+        when(mockColumn1.getRoles()).thenReturn(Arrays.asList("r1", "r2", "r3"));
+        when(mockColumn1.isReftext()).thenReturn(false);
+        when(mockColumn1.getStyle()).thenReturn("STYLE");
+        when(mockColumn1.getTable()).thenReturn(null);
+        when(mockColumn1.getColumnNumber()).thenReturn(2);
+        when(mockColumn1.getWidth()).thenReturn(550);
+        when(mockColumn1.getHorizontalAlignment()).thenReturn(Table.HorizontalAlignment.CENTER);
+        when(mockColumn1.getVerticalAlignment()).thenReturn(Table.VerticalAlignment.MIDDLE);
+        return mockColumn1;
+    }
+    // end::generated-code[]
 }
