6ce787ab5c981779d6bcf4270e5fe202bd5e37a5
Jeremie Bresson <dev@jmini.fr>
2017-11-30 09:17:11 +0100

    docs: add a description for the new converters "assert-code" and "mock-code"

---
+++ code-converter/assert-code-converter/src/test/java/fr/jmini/asciidoctorj/converter/assertcode/AssertCodeGeneratorExampleTest.java
@@ -0,0 +1,146 @@
+package fr.jmini.asciidoctorj.converter.assertcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.asciidoctor.ast.Block;
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.ast.Title;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import fr.jmini.asciidoctorj.converter.code.CodeTestingUtility;
+
+/*
+See Input AciiDoc Text in @link{fr.jmini.asciidoctorj.converter.mockcode.MockCodeGeneratorExampleTest}
+ */
+public class AssertCodeGeneratorExampleTest {
+
+    @Test
+    public void testAst() throws Exception {
+        Document mockDocument = createMock();
+
+        AssertCodeGenerator generator = new AssertCodeGenerator();
+        StringBuilder sb = new StringBuilder();
+        generator.createDocumentCode(sb, mockDocument);
+        CodeTestingUtility.testGeneratedCode(sb.toString(), this.getClass());
+
+        checkAst(mockDocument);
+    }
+
+    public Document createMock() {
+        Document mockDocument1 = mock(Document.class);
+        when(mockDocument1.getId()).thenReturn(null);
+        when(mockDocument1.getNodeName()).thenReturn("document");
+        when(mockDocument1.getContext()).thenReturn("document");
+        when(mockDocument1.getDocument()).thenReturn(mockDocument1);
+        when(mockDocument1.isInline()).thenReturn(false);
+        when(mockDocument1.isBlock()).thenReturn(true);
+        ImmutableMap<String, Object> map1 = ImmutableMap.<String, Object>builder()
+                .put("tip-caption", "Tip")
+                .put("filetype", "html")
+                .put("doctitle", "My page")
+                .put("doctype", "article")
+                .build();
+        when(mockDocument1.getAttributes()).thenReturn(map1);
+        when(mockDocument1.getRoles()).thenReturn(Collections.emptyList());
+        when(mockDocument1.isReftext()).thenReturn(false);
+        when(mockDocument1.getTitle()).thenReturn(null);
+        when(mockDocument1.getStyle()).thenReturn(null);
+        when(mockDocument1.getLevel()).thenReturn(0);
+        when(mockDocument1.getContentModel()).thenReturn("compound");
+        when(mockDocument1.getSourceLocation()).thenReturn(null);
+        when(mockDocument1.getSubstitutions()).thenReturn(Collections.emptyList());
+        Block mockBlock1 = mock(Block.class);
+        when(mockBlock1.getId()).thenReturn(null);
+        when(mockBlock1.getNodeName()).thenReturn("paragraph");
+        when(mockBlock1.getContext()).thenReturn("paragraph");
+        when(mockBlock1.getDocument()).thenReturn(mockDocument1);
+        when(mockBlock1.isInline()).thenReturn(false);
+        when(mockBlock1.isBlock()).thenReturn(true);
+        when(mockBlock1.getAttributes()).thenReturn(Collections.emptyMap());
+        when(mockBlock1.getRoles()).thenReturn(Collections.emptyList());
+        when(mockBlock1.isReftext()).thenReturn(false);
+        when(mockBlock1.getTitle()).thenReturn(null);
+        when(mockBlock1.getStyle()).thenReturn(null);
+        when(mockBlock1.getLevel()).thenReturn(0);
+        when(mockBlock1.getContentModel()).thenReturn("simple");
+        when(mockBlock1.getSourceLocation()).thenReturn(null);
+        when(mockBlock1.getSubstitutions()).thenReturn(Arrays.asList("specialcharacters", "quotes", "attributes", "replacements", "macros", "post_replacements"));
+        when(mockBlock1.getBlocks()).thenReturn(Collections.emptyList());
+        when(mockBlock1.getLines()).thenReturn(Collections.singletonList("Some text"));
+        when(mockBlock1.getSource()).thenReturn("Some text");
+        when(mockDocument1.getBlocks()).thenReturn(Collections.singletonList(mockBlock1));
+        Title mockTitle1 = mock(Title.class);
+        when(mockTitle1.getMain()).thenReturn("My page");
+        when(mockTitle1.getSubtitle()).thenReturn(null);
+        when(mockTitle1.getCombined()).thenReturn("My page");
+        when(mockTitle1.isSanitized()).thenReturn(false);
+        when(mockDocument1.getStructuredDoctitle()).thenReturn(mockTitle1);
+        when(mockDocument1.getDoctitle()).thenReturn("My page");
+        ImmutableMap<Object, Object> map2 = ImmutableMap.<Object, Object>builder()
+                .put("header_footer", false)
+                .put("attributes", "{}")
+                .build();
+        when(mockDocument1.getOptions()).thenReturn(map2);
+        return mockDocument1;
+    }
+
+    // tag::generated-code[]
+    public void checkAst(Document astDocument) {
+        Document document1 = astDocument;
+        assertThat(document1.getId()).isNull();
+        assertThat(document1.getNodeName()).isEqualTo("document");
+        assertThat(document1.getContext()).isEqualTo("document");
+        assertThat(document1.getDocument()).isSameAs(document1);
+        assertThat(document1.isInline()).isFalse();
+        assertThat(document1.isBlock()).isTrue();
+        assertThat(document1.getAttributes()).containsEntry("tip-caption", "Tip")
+                .containsEntry("filetype", "html")
+                .containsEntry("doctitle", "My page")
+                .containsEntry("doctype", "article");
+        assertThat(document1.getRoles()).isEmpty();
+        assertThat(document1.isReftext()).isFalse();
+        assertThat(document1.getTitle()).isNull();
+        assertThat(document1.getStyle()).isNull();
+        assertThat(document1.getLevel()).isEqualTo(0);
+        assertThat(document1.getContentModel()).isEqualTo("compound");
+        assertThat(document1.getSourceLocation()).isNull();
+        assertThat(document1.getSubstitutions()).isEmpty();
+        assertThat(document1.getBlocks()).hasSize(1);
+        Block block1 = (Block) document1.getBlocks()
+                .get(0);
+        assertThat(block1.getId()).isNull();
+        assertThat(block1.getNodeName()).isEqualTo("paragraph");
+        assertThat(block1.getContext()).isEqualTo("paragraph");
+        assertThat(block1.getDocument()).isSameAs(document1);
+        assertThat(block1.isInline()).isFalse();
+        assertThat(block1.isBlock()).isTrue();
+        assertThat(block1.getAttributes()).isEmpty();
+        assertThat(block1.getRoles()).isEmpty();
+        assertThat(block1.isReftext()).isFalse();
+        assertThat(block1.getTitle()).isNull();
+        assertThat(block1.getStyle()).isNull();
+        assertThat(block1.getLevel()).isEqualTo(0);
+        assertThat(block1.getContentModel()).isEqualTo("simple");
+        assertThat(block1.getSourceLocation()).isNull();
+        assertThat(block1.getSubstitutions()).containsExactly("specialcharacters", "quotes", "attributes", "replacements", "macros", "post_replacements");
+        assertThat(block1.getBlocks()).isEmpty();
+        assertThat(block1.getLines()).containsExactly("Some text");
+        assertThat(block1.getSource()).isEqualTo("Some text");
+        Title title1 = document1.getStructuredDoctitle();
+        assertThat(title1.getMain()).isEqualTo("My page");
+        assertThat(title1.getSubtitle()).isNull();
+        assertThat(title1.getCombined()).isEqualTo("My page");
+        assertThat(title1.isSanitized()).isFalse();
+        assertThat(document1.getDoctitle()).isEqualTo("My page");
+        assertThat(document1.getOptions()).containsEntry("header_footer", false)
+                .containsEntry("attributes", "{}");
+    }
+    // end::generated-code[]
+}
---
+++ code-converter/mock-code-converter/src/test/java/fr/jmini/asciidoctorj/converter/mockcode/MockCodeGeneratorExampleTest.java
@@ -0,0 +1,99 @@
+package fr.jmini.asciidoctorj.converter.mockcode;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.asciidoctor.ast.Block;
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.ast.Title;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import fr.jmini.asciidoctorj.converter.code.CodeTestingUtility;
+
+/*
+Input AciiDoc Text:
+----
+// tag::input[]
+= My page
+
+Some text
+// end::input[]
+----
+ */
+public class MockCodeGeneratorExampleTest {
+
+    @Test
+    public void testAst() throws Exception {
+        Document mockDocument = createMock();
+
+        MockCodeGenerator generator = new MockCodeGenerator();
+        StringBuilder sb = new StringBuilder();
+        generator.createDocumentCode(sb, mockDocument);
+        CodeTestingUtility.testGeneratedCode(sb.toString(), this.getClass());
+    }
+
+    // tag::generated-code[]
+    public Document createMock() {
+        Document mockDocument1 = mock(Document.class);
+        when(mockDocument1.getId()).thenReturn(null);
+        when(mockDocument1.getNodeName()).thenReturn("document");
+        when(mockDocument1.getContext()).thenReturn("document");
+        when(mockDocument1.getDocument()).thenReturn(mockDocument1);
+        when(mockDocument1.isInline()).thenReturn(false);
+        when(mockDocument1.isBlock()).thenReturn(true);
+        ImmutableMap<String, Object> map1 = ImmutableMap.<String, Object>builder()
+                .put("tip-caption", "Tip")
+                .put("filetype", "html")
+                .put("doctitle", "My page")
+                .put("doctype", "article")
+                .build();
+        when(mockDocument1.getAttributes()).thenReturn(map1);
+        when(mockDocument1.getRoles()).thenReturn(Collections.emptyList());
+        when(mockDocument1.isReftext()).thenReturn(false);
+        when(mockDocument1.getTitle()).thenReturn(null);
+        when(mockDocument1.getStyle()).thenReturn(null);
+        when(mockDocument1.getLevel()).thenReturn(0);
+        when(mockDocument1.getContentModel()).thenReturn("compound");
+        when(mockDocument1.getSourceLocation()).thenReturn(null);
+        when(mockDocument1.getSubstitutions()).thenReturn(Collections.emptyList());
+        Block mockBlock1 = mock(Block.class);
+        when(mockBlock1.getId()).thenReturn(null);
+        when(mockBlock1.getNodeName()).thenReturn("paragraph");
+        when(mockBlock1.getContext()).thenReturn("paragraph");
+        when(mockBlock1.getDocument()).thenReturn(mockDocument1);
+        when(mockBlock1.isInline()).thenReturn(false);
+        when(mockBlock1.isBlock()).thenReturn(true);
+        when(mockBlock1.getAttributes()).thenReturn(Collections.emptyMap());
+        when(mockBlock1.getRoles()).thenReturn(Collections.emptyList());
+        when(mockBlock1.isReftext()).thenReturn(false);
+        when(mockBlock1.getTitle()).thenReturn(null);
+        when(mockBlock1.getStyle()).thenReturn(null);
+        when(mockBlock1.getLevel()).thenReturn(0);
+        when(mockBlock1.getContentModel()).thenReturn("simple");
+        when(mockBlock1.getSourceLocation()).thenReturn(null);
+        when(mockBlock1.getSubstitutions()).thenReturn(Arrays.asList("specialcharacters", "quotes", "attributes", "replacements", "macros", "post_replacements"));
+        when(mockBlock1.getBlocks()).thenReturn(Collections.emptyList());
+        when(mockBlock1.getLines()).thenReturn(Collections.singletonList("Some text"));
+        when(mockBlock1.getSource()).thenReturn("Some text");
+        when(mockDocument1.getBlocks()).thenReturn(Collections.singletonList(mockBlock1));
+        Title mockTitle1 = mock(Title.class);
+        when(mockTitle1.getMain()).thenReturn("My page");
+        when(mockTitle1.getSubtitle()).thenReturn(null);
+        when(mockTitle1.getCombined()).thenReturn("My page");
+        when(mockTitle1.isSanitized()).thenReturn(false);
+        when(mockDocument1.getStructuredDoctitle()).thenReturn(mockTitle1);
+        when(mockDocument1.getDoctitle()).thenReturn("My page");
+        ImmutableMap<Object, Object> map2 = ImmutableMap.<Object, Object>builder()
+                .put("header_footer", false)
+                .put("attributes", "{}")
+                .build();
+        when(mockDocument1.getOptions()).thenReturn(map2);
+        return mockDocument1;
+    }
+    // end::generated-code[]
+}
